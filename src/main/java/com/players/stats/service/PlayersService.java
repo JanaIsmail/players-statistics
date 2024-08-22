c97d0f27e7b1ff244991fe03e2bcd6fb76bdcebc
Jana Ismail <jana.ismail@telecom-paris.fr>
2024-08-23 01:01:57 +0200

    fix bean initialization issue

--- src/main/java/com/players/stats/service/PlayersService.java
+++ src/main/java/com/players/stats/service/PlayersService.java
@@ -4,6 +4,7 @@ import com.players.stats.dto.Player;
 import com.players.stats.dto.Players;
 import com.players.stats.dto.PlayersStatistics;
 import com.players.stats.exception.PlayerNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -14,6 +15,7 @@ public class PlayersService {
 
     private final List<Player> playersList;
 
+    @Autowired
     public PlayersService(Players players) {
         this.playersList = (null != players) ? players.getPlayers() : new ArrayList<>();
     }
