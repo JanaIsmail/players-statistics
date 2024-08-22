4bde5dc4b340e5b63ee09d68c2eee4d7ee24a8f2
Jana Ismail <jana.ismail@telecom-paris.fr>
2024-08-23 01:07:03 +0200

    fix bean initialization issue

--- src/main/java/com/players/stats/config/ServiceConfiguration.java
+++ src/main/java/com/players/stats/config/ServiceConfiguration.java
@@ -2,12 +2,14 @@ package com.players.stats.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.players.stats.dto.Players;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class ServiceConfiguration {
 
@@ -16,11 +18,11 @@ public class ServiceConfiguration {
     @Bean
     public Players readPlayersFile() {
         // ideally we would have a database
-        Players players = null;
+        Players players = Players.builder().build();
         try {
             players = mapper.readValue(new File("src/main/resources/players.json"), Players.class);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
         return players;
     }
